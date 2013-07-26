9c82a18f45be0dd0f8c3ce111320c2845561593f
Aaron Cronin <aaron@freeagent.com>
2013-07-26 16:11:51 +0100

    PATH Cleanup.

--- zsh/.oh-my-zsh/custom/exports.sh
+++ zsh/.oh-my-zsh/custom/exports.sh
@@ -1,4 +1,4 @@
-export PATH=/Users/aaron/.gem/ruby/1.9.3/bin:/Users/aaron/.rbenv/versions/1.9.3-p448/lib/ruby/gems/1.9.1/bin:/Users/aaron/.homebrew/sbin:/Users/aaron/.homebrew/bin:/usr/local/bin:/opt/X11/bin:/usr/X11/bin:/usr/local/git/bin:$PATH
+export PATH=~/.homebrew/sbin:~/.homebrew/bin:$PATH
 
 export RUBY_HEAP_MIN_SLOTS=600000
 export RUBY_HEAP_SLOTS_INCREMENT=250000
