573e9e0014b94a052f8fdb8d56bfb1e6a0a36c4a
Aaron Cronin <aaron@freeagent.com>
2013-08-02 11:38:01 +0100

    Autojump and mysqlram

--- zsh/.oh-my-zsh/custom/aliases.sh
+++ zsh/.oh-my-zsh/custom/aliases.sh
@@ -3,4 +3,4 @@ alias vim='mvim -v'
 alias story="~/dev/storyid.rb"
 alias prod_sha="curl -I https://fac.freeagent.com/ 2>/dev/null | grep -E 'X-(Rev)' | cut -d ' ' -f2 | dos2unix"
 alias prod_diff="git diff $(prod_sha) --patience"
-
+alias mysqlram='diskutil erasevolume HFS+ "ramdisk" `hdiutil attach -nomount ram://1048576` && cp -R `brew --prefix`/var/mysql/* /Volumes/ramdisk/ && mysql.server start'
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -6,7 +6,6 @@ ZSH_THEME="robbyrussell"
 
 COMPLETION_WAITING_DOTS="true"
 
-plugins=(git rails ruby osx bundler)
 
 source /usr/local/share/chruby/chruby.sh
 chruby 1.9.3-p385-perf
@@ -17,3 +16,4 @@ source $ZSH/custom/aliases.sh
 source $ZSH/custom/fns.sh
 source $ZSH/custom/autorun.sh
 
+plugins=(git rails ruby osx bundler autojump)
