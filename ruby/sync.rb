67278b5abc72beefe86847ffe52f2c8f3f36e82c
Aaron Cronin <cronin@include.cat>
2013-08-12 11:16:49 +0100

    .ruby-version tracked

--- ruby/sync.rb
+++ ruby/sync.rb
@@ -2,6 +2,7 @@ class RubySync < Struct.new(:dir)
 
   def sync
     replace_with_symlink '~/.pryrc', (dir + '.pryrc')
+    replace_with_symlink '~/.ruby-version', (dir + '~/.ruby-version')
   end
 
 end
