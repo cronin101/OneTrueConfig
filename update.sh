d85af7e8b6e34f8c3229b2a0a7aed3164647683a
Aaron Cronin <cronin@include.cat>
2013-09-09 10:58:35 +0100

    Update and add OpenCL

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-git submodule update --init --recursive
-git submodule foreach git pull origin master
+#git submodule update --init --recursive
+#git submodule foreach git fetch
+#git submodule foreach git checkout master
+#git submodule foreach git pull origin master
+
+git submodule update --recursive
