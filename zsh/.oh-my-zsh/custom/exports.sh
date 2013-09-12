7de1007a774708d33dd58a18735a2e38d1d198ac
Aaron Cronin <cronin@include.cat>
2013-09-12 18:30:25 +0100

    Fix exports now that I no longer use mvim

--- zsh/.oh-my-zsh/custom/exports.sh
+++ zsh/.oh-my-zsh/custom/exports.sh
@@ -4,8 +4,6 @@ export RUBY_HEAP_MIN_SLOTS=600000
 export RUBY_HEAP_SLOTS_INCREMENT=250000
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_FREE_MIN=500000
-export EDITOR='mvim -v'
+export EDITOR='vim -'
 export GNUTERM='x11'
-
-#set env DYLD_INSERT_LIBRARIES /usr/local/lib/libtcmalloc_minimal.dylib
 
