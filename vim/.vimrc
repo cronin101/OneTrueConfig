09789eb8f3819bb206887ff5e7e723d434523900
Aaron Cronin <cronin@include.cat>
2014-04-03 22:28:06 +0100

    Routine update

--- vim/.vimrc
+++ vim/.vimrc
@@ -186,7 +186,7 @@ endif
 " Rspec.vim mappings
 map <Leader>t :call RunCurrentSpecFile()<CR>
 map <Leader>s :call RunNearestSpec()<CR>
-map <Leader>l :call RunLastSpec()<CR>
+map <Leader>r :call RunLastSpec()<CR>
 map <Leader>a :call RunAllSpecs()<CR>
 let g:rspec_command = "Dispatch rspec {spec}"
 
--- zsh/.oh-my-zsh/custom/exports.sh
+++ zsh/.oh-my-zsh/custom/exports.sh
@@ -1,9 +1,8 @@
 export PATH=$HOME/.cabal/bin:$HOME/Library/Haskell/bin:/usr/local/sbin:/usr/local/bin:$PATH
+export CLASSPATH=$CLASSPATH:$HOME/Dev/Java/ADBS/build
 
-export RUBY_HEAP_MIN_SLOTS=600000
 export RUBY_HEAP_SLOTS_INCREMENT=250000
 export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_FREE_MIN=500000
 export EDITOR='vim'
 export GNUTERM='x11'
 
